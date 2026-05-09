5e636591c065612dcd26914f7d23d65cebb62112
redrhino-dot <kkeane@gmail.com>
2026-05-09 12:08:21 +0100

    Update config.js

--- data/config.js
+++ data/config.js
@@ -20,14 +20,12 @@ const CONFIG = {
     "Kris": "1940",
     "Doug": "1030"
   },
-  
-  "openingStandings: [
-  { name: "Kris",   points: 150 },
-  { name: "Graham", points: 149 },
-  { name: "Jon",    points: 149 },
-  { name: "Doug",   points: 115 },
-],
-
+  "openingStandings": [
+    { "name": "Kris",   "points": 150 },
+    { "name": "Graham", "points": 149 },
+    { "name": "Jon",    "points": 149 },
+    { "name": "Doug",   "points": 115 }
+  ],
   "teamAliases": {
     "Hibs": "Hibernian",
     "Huns": "Rangers",
