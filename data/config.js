79d1ce0dcc37df47c7a96b7665468aaebeba9399
redrhino-dot <kkeane@gmail.com>
2026-07-30 00:57:31 +0100

    Update config.js

--- data/config.js
+++ data/config.js
@@ -43,7 +43,7 @@ const CONFIG = {
     "Huns": "Rangers",
     "Celtic": "Celtic",
     "St M": "St Mirren",
-    "St J": "St Johnstone";
+    "St J": "St Johnstone",
     "Dons": "Aberdeen",
     "Yinited": "Dundee United",
     "Utd": "Dundee United",
